afff2ec6496601857bd18340dda12a5df7a924df
Vincent Le <vinnyoodles@gmail.com>
2016-03-14 11:15:28 -0400

    fix wall collision bug

--- js/snake.js
+++ js/snake.js
@@ -47,10 +47,10 @@ var Snake = function() {
     this.body.unshift(head);
   }
   this.collidingWall = function(canvas){
-    return (this.head().x <= 0                               ) ||
-           (this.head().y <= 0                               ) ||
-           (this.head().x + this.head().size >= canvas.width ) ||
-           (this.head().y + this.head().size >= canvas.height);
+    return (this.head().x < 0                               ) ||
+           (this.head().y < 0                               ) ||
+           (this.head().x + this.head().size > canvas.width ) ||
+           (this.head().y + this.head().size > canvas.height);
   }
   this.collidingObjects = function(objects) {
     var head = this.head();
